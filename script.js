b87ca28c1c5377e46199d4b0f87c219fde3902cc
NataliaFedakova <117270929+NataliaFedakova@users.noreply.github.com>
2024-09-13 17:35:53 +0200

    slanetorty

---
+++ script.js
@@ -0,0 +1,34 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const burger = document.querySelector('.burger');
+    const navLinks = document.querySelector('.nav-links');
+
+    burger.addEventListener('click', () => {
+        navLinks.classList.toggle('active');
+        burger.classList.toggle('active');
+    });
+});
+
+const carouselContainer = document.querySelector('.carousel-container');
+const prevButton = document.querySelector('.carousel-button.prev');
+const nextButton = document.querySelector('.carousel-button.next');
+let currentIndex = 0;
+
+function showSlide(index) {
+    const slides = document.querySelectorAll('.carousel-item');
+    if (index >= slides.length) {
+        currentIndex = 0;
+    } else if (index < 0) {
+        currentIndex = slides.length - 1;
+    } else {
+        currentIndex = index;
+    }
+    carouselContainer.style.transform = `translateX(-${currentIndex * 100}%)`;
+}
+
+nextButton.addEventListener('click', () => {
+    showSlide(currentIndex + 1);
+});
+
+prevButton.addEventListener('click', () => {
+    showSlide(currentIndex - 1);
+});
